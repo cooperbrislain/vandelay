673ae9c92b1623976796ed795b0af7ade81bda2d
Cooper Brislain <cbrislain@gmail.com>
2020-12-30 10:22:15 -0800

    .env support

--- includes/config.php
+++ includes/config.php
@@ -3,6 +3,6 @@
 error_reporting(E_ALL);
 ini_set('html_errors', 1);
 ini_set('display_errors', 1);;
-require_once("../wp-config.php");
+require_once("./.env");
 $source = mysqli_connect("127.0.0.1", DB_USER, DB_PASSWORD, "migrated_from_old_site");
 $target = mysqli_connect("127.0.0.1", DB_USER, DB_PASSWORD, DB_NAME);
