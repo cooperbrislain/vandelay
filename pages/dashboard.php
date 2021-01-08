27418b6a696a1575d339f79c3014e9e3a2280fa1
Cooper Brislain <cbrislain@gmail.com>
2021-01-08 11:59:03 -0800

    dashboard counts

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -2,5 +2,18 @@
 require_once('includes/opts.php');
 require_once('includes/data.php');
 
+$q = <<<QUERY
+    SELECT COUNT(venues.v_id) AS count FROM venues 
+    LEFT JOIN v_import ON venues.v_id == v_import.v_id 
+    GROUP BY v_import.status
+QUERY;
+
 ?>
-Welcome!
+<div class="container">
+    <dl>
+        <dt>Total Items</dt><dd></dd>
+        <dt>Items Imported</dt><dd></dd>
+        <dt>Items Remaining</dt><dd></dd>
+        <dt>Errors</dt><dd></dd>
+    </dl>
+</div>
