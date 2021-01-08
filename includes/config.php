581290aa093bc97b30c9dd997f4b6ae3d5de320e
Cooper Brislain <cbrislain@gmail.com>
2021-01-08 10:34:08 -0800

    catching up

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,11 @@
 <?php
+
 use Dotenv\Dotenv;
 
 set_include_path(get_include_path() .
     PATH_SEPARATOR . '../vendor' .
     PATH_SEPARATOR . '..' .
-    PATH_SEPARATOR . '/home/t04ucpbct3k8/smallweddings.com',
+    PATH_SEPARATOR . '/home/t04ucpbct3k8/smallweddings.com'.
     PATH_SEPARATOR . '/home/t04ucpbct3k8/smallweddings.com/vandelay'
 );
 echo get_include_path();
