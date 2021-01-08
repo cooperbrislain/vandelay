d88091639b1abb96b6b027a50f470d1116212095
Cooper Brislain <cbrislain@gmail.com>
2021-01-08 10:55:37 -0800

    stuff

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 use Dotenv\Dotenv;
 
 set_include_path(get_include_path() .
-    PATH_SEPARATOR . '../vendor' .
+    PATH_SEPARATOR . '/vendor' .
     PATH_SEPARATOR . '..' .
     PATH_SEPARATOR . '/home/t04ucpbct3k8/smallweddings.com'.
     PATH_SEPARATOR . '/home/t04ucpbct3k8/smallweddings.com/vandelay'
@@ -13,7 +13,7 @@ error_reporting(E_ALL);
 ini_set('html_errors', 1);
 ini_set('display_errors', 1);;
 
-$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $source = mysqli_connect("127.0.0.1", $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], "migrated_from_old_site");
--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $opts = [
     'start'=>0,
     'limit'=>5
 ];
-require_once('includes/config.php');
+require_once('config.php');
 require_once('includes/functions.php');
 require_once('includes/opts.php');
 require_once('includes/data.php');
