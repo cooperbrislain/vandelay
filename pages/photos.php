d0655931d1668c4ec7fe41cfb2d44534461efffc
Cooper Brislain <cbrislain@gmail.com>
2021-01-08 18:59:00 -0800

    group photos by venue

--- pages/photos.php
+++ pages/photos.php
@@ -5,6 +5,7 @@ require_once('includes/data.php');
 $q = <<<QUERY
     SELECT * FROM venue_photos
     INNER JOIN v_import ON venue_photos.v_id = v_import.v_id
+    GROUP BY venue_photos.v_id
 QUERY;
 $res = doq($q);
 while ($row = mysqli_fetch_assoc($res)) {
