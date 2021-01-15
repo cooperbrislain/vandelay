c3f7cd4c57860a55e441afb2840f83785ca501d2
Cooper Brislain <cbrislain@gmail.com>
2021-01-15 09:36:59 -0800

    bootstrap

--- includes/head.php
+++ includes/head.php
@@ -1,8 +1,12 @@
+<?php // NOT TO BE CONFUSED WITH HEADER ?>
 <head>
     <title>Vandelay</title>
     <link rel="stylesheet" href="assets/style.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"
             integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"
+            integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf"
             crossorigin="anonymous"></script>
     <script type="application/javascript" src="assets/script.js"></script>
 </head>
