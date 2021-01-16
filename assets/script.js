cb54c2241968792c59f83ff0bd1313948877d7f8
Cooper Brislain <cbrislain@gmail.com>
2021-01-15 22:00:00 -0800

    getting scripts sorted

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,1 @@
-jQuery(document).ready(() => {
-    $('table').on('click', 'td.v_id', (e) => {
-        let url = "?page=venue&v_id="+$.trim($(e.target).text());
-        window.open(url, '_blank');
-    });
-});
+console.log('Welcome to Vandelay');
